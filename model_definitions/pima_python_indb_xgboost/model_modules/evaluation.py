7aeb9d569b0134052b52436b7829bb2d701edcae
hnaumanyousaf <h.nauman573@gmail.com>
2026-02-13 16:05:35 +0000

    Added InDB XGBOOST model Test

--- model_definitions/pima_python_indb_xgboost/model_modules/evaluation.py
+++ model_definitions/pima_python_indb_xgboost/model_modules/evaluation.py
@@ -1,133 +1,122 @@
 from sklearn.metrics import confusion_matrix
-from teradataml import (
-    DataFrame,
-    copy_to_sql,
-    ScaleTransform,
-    XGBoostPredict,
-    ConvertTo,
+import matplotlib.pyplot as plt
+from teradataml import(
+    DataFrame, 
+    copy_to_sql, 
+    get_context, 
+    get_connection, 
+    ScaleTransform, 
+    XGBoostPredict, 
+    ConvertTo, 
     ClassificationEvaluator,
-    ROC
+    ROC,
+    Shap
 )
-from tmo import (
+from aoa import (
     record_evaluation_stats,
+    save_plot,
     tmo_create_context,
     ModelContext
 )
-from collections import Counter
 
-import matplotlib.pyplot as plt
+import joblib
 import json
+import numpy as np
 import pandas as pd
 import os
 
-def traverse_tree(tree, feature_counter):
-    if 'split_' in tree and 'attr_' in tree['split_']:
-        feature_counter[tree['split_']['attr_']] += 1
-    if 'leftChild_' in tree:
-        traverse_tree(tree['leftChild_'], feature_counter)
-    if 'rightChild_' in tree:
-        traverse_tree(tree['rightChild_'], feature_counter)
 
-
-def compute_feature_importance(trees_json):
-    feature_counter = Counter()
-    for tree_json in trees_json:
-        tree = json.loads(tree_json)
-        traverse_tree(tree, feature_counter)
-    total_splits = sum(feature_counter.values())
-    feature_importance = {
-        feature: count / total_splits for feature, count in feature_counter.items()}
-    return feature_importance
-
-
-def plot_feature_importance(fi, img_filename):
-    feat_importances = pd.Series(fi)
-    feat_importances.nlargest(10).plot(
-        kind='barh').set_title('Feature Importance')
-    fig = plt.gcf()
-    fig.savefig(img_filename, dpi=500)
-    plt.clf()
-
-
+# Define function to plot a confusion matrix from given data
 def plot_confusion_matrix(cf, img_filename):
+    import matplotlib.pyplot as plt
     fig, ax = plt.subplots(figsize=(7.5, 7.5))
     ax.matshow(cf, cmap=plt.cm.Blues, alpha=0.3)
     for i in range(cf.shape[0]):
         for j in range(cf.shape[1]):
-            ax.text(x=j, y=i, s=cf[i, j], va='center',
-                    ha='center', size='xx-large')
-    ax.set_xlabel('Predicted labels')
-    ax.set_ylabel('True labels')
-    ax.set_title('Confusion Matrix')
+            ax.text(x=j, y=i,s=cf[i, j], va='center', ha='center', size='xx-large')
+    ax.set_xlabel('Predicted labels');
+    ax.set_ylabel('True labels'); 
+    ax.set_title('Confusion Matrix');
     fig = plt.gcf()
     fig.savefig(img_filename, dpi=500)
     plt.clf()
 
 
 def plot_roc_curve(roc_out, img_filename):
-    auc = roc_out.result.to_pandas().reset_index()['AUC'][0]
-    roc_results = roc_out.output_data.to_pandas()
-    plt.plot(roc_results['fpr'], roc_results['tpr'],
-             color='darkorange', lw=2, label='ROC curve (AUC = %0.2f)' % 0.27)
-    plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
-    plt.xlim([0.0, 1.0])
-    plt.ylim([0.0, 1.05])
-    plt.xlabel('False Positive Rate')
-    plt.ylabel('True Positive Rate')
-    plt.title('Receiver Operating Characteristic (ROC) Curve')
-    plt.legend(loc="lower right")
-    fig = plt.gcf()
-    fig.savefig(img_filename, dpi=500)
-    plt.clf()
-
+    from teradataml import Figure
+    figure = Figure(width=500, height=400, heading="Receiver Operating Characteristic (ROC) Curve")
+    auc = roc_out.result.get_values()[0][0]
+    plot = roc_out.output_data.plot(
+        x=roc_out.output_data.fpr,
+        y=[roc_out.output_data.tpr, roc_out.output_data.fpr],
+        xlabel='False Positive Rate',
+        ylabel='True Positive Rate',
+        color='carolina blue',
+        figure=figure,
+        legend=[f'XGBoost AUC = {round(auc, 4)}', 'AUC Baseline'],
+        legend_style='lower right',
+        grid_linestyle='--',
+        grid_linewidth=0.5
+    )
+    plot.save(img_filename)
+    # plot.show()
+    # fig = plt.gcf()
+    # fig.savefig(img_filename, dpi=500)
+    # plt.clf()    
 
 def evaluate(context: ModelContext, **kwargs):
 
     tmo_create_context()
 
-    print(f"Loading model from table model_{context.model_version}")
-    model = DataFrame(f"model_{context.model_version}")
+    # Load the trained model from SQL
+    model = DataFrame(f"model_${context.model_version}")
 
+    feature_names = context.dataset_info.feature_names
     target_name = context.dataset_info.target_names[0]
     entity_key = context.dataset_info.entity_key
 
+    # Load the test data from Teradata
     test_df = DataFrame.from_query(context.dataset_info.sql)
 
     # Scaling the test set
-    print(f"Loading scaler from table scaler_{context.model_version}")
-    scaler = DataFrame(f"scaler_{context.model_version}")
+    print ("Loading scaler...")
+    scaler = DataFrame(f"scaler_${context.model_version}")
 
     scaled_test = ScaleTransform(
         data=test_df,
         object=scaler,
-        accumulate=[target_name, entity_key]
+        accumulate = [target_name,entity_key]
     )
 
     print("Evaluating...")
+    # Make predictions using the XGBoostPredict function
     predictions = XGBoostPredict(
         object=model,
         newdata=scaled_test.result,
-        model_type='Classification',
+        model_type = 'Classification',
         accumulate=target_name,
         id_column=entity_key,
         output_prob=True,
-        output_responses=['0', '1'],
-        object_order_column=['task_index', 'tree_num',
-                             'iter', 'class_num', 'tree_order']
+        output_responses=['0','1'],
+        object_order_column=['task_index', 'tree_num', 'iter', 'class_num', 'tree_order']
     )
 
+    # Convert the predicted data into the specified format
     predicted_data = ConvertTo(
-        data=predictions.result,
-        target_columns=[target_name, 'Prediction'],
-        target_datatype=["INTEGER"]
+        data = predictions.result,
+        target_columns = [target_name,'Prediction'],
+        target_datatype = ["INTEGER"]
     )
 
+    # Evaluate classification metrics using ClassificationEvaluator
     ClassificationEvaluator_obj = ClassificationEvaluator(
         data=predicted_data.result,
         observation_column=target_name,
         prediction_column='Prediction',
         num_labels=2
     )
+
+     # Extract and store evaluation metrics
 
     metrics_pd = ClassificationEvaluator_obj.output_data.to_pandas()
 
@@ -144,15 +133,18 @@ def evaluate(context: ModelContext, **kwargs):
         'Weighted-F1': '{:.2f}'.format(metrics_pd.MetricValue[9]),
     }
 
+     # Save evaluation metrics to a JSON file
     with open(f"{context.artifact_output_path}/metrics.json", "w+") as f:
         json.dump(evaluation, f)
 
-    cm = confusion_matrix(predicted_data.result.to_pandas()[
-                          'HasDiabetes'], predicted_data.result.to_pandas()['Prediction'])
+    # Generate and save confusion matrix plot
+    cm_df = ClassificationEvaluator_obj.result
+    cm_df = cm_df.select(['CLASS_1','CLASS_2'])
+    cm_df_t = cm_df.to_pandas().T
+    # cm = confusion_matrix(predicted_data.result.to_pandas()['HasDiabetes'], predicted_data.result.to_pandas()['Prediction'])
+    plot_confusion_matrix(cm_df_t.values, f"{context.artifact_output_path}/confusion_matrix")
 
-    plot_confusion_matrix(
-        cm, f"{context.artifact_output_path}/confusion_matrix")
-
+    # Generate and save ROC curve plot
     roc_out = ROC(
         data=predictions.result,
         probability_column='Prob_1',
@@ -160,31 +152,17 @@ def evaluate(context: ModelContext, **kwargs):
         positive_class='1',
         num_thresholds=1000
     )
-
     plot_roc_curve(roc_out, f"{context.artifact_output_path}/roc_curve")
 
-    # Calculate feature importance and generate plot
-    try:
-        model_pdf = model.result.to_pandas()['classification_tree']
-        feature_importance = compute_feature_importance(model_pdf)
-        feature_importance_df = pd.DataFrame(
-            list(feature_importance.items()), columns=['Feature', 'Importance'])
-        plot_feature_importance(
-            feature_importance, f"{context.artifact_output_path}/feature_importance")
-    except:
-        feature_importance = {}
-
     predictions_table = "predictions_tmp"
-    copy_to_sql(df=predicted_data.result, table_name=predictions_table,
-                index=False, if_exists="replace", temporary=True)
+    copy_to_sql(df=predicted_data.result, table_name=predictions_table, index=False, if_exists="replace", temporary=True)
 
     # calculate stats if training stats exist
     if os.path.exists(f"{context.artifact_input_path}/data_stats.json"):
         record_evaluation_stats(
             features_df=test_df,
-            predicted_df=DataFrame.from_query(
-                f"SELECT * FROM {predictions_table}"),
-            feature_importance=feature_importance,
+            predicted_df=DataFrame.from_query(f"SELECT * FROM {predictions_table}"),
+            # feature_importance=feature_importance,
             context=context
         )
 
--- model_definitions/pima_python_indb_xgboost/model_modules/scoring.py
+++ model_definitions/pima_python_indb_xgboost/model_modules/scoring.py
@@ -2,77 +2,85 @@ from teradataml import (
     copy_to_sql,
     DataFrame,
     XGBoostPredict,
-    ScaleTransform
+    ScaleTransform,
+    ConvertTo,
+    translate
 )
-from tmo import (
+from aoa import (
     record_scoring_stats,
     tmo_create_context,
     ModelContext
 )
 import pandas as pd
+from teradatasqlalchemy import INTEGER
 
 
 def score(context: ModelContext, **kwargs):
 
     tmo_create_context()
 
-    print(f"Loading model from table model_{context.model_version}")
-    model = DataFrame(f"model_{context.model_version}")
+    # Load the trained model from SQL
+    model = DataFrame(f"model_${context.model_version}")
 
+    # Extract feature names, target name, and entity key from the context
     feature_names = context.dataset_info.feature_names
     target_name = context.dataset_info.target_names[0]
     entity_key = context.dataset_info.entity_key
 
+    # Load the test dataset
     test_df = DataFrame.from_query(context.dataset_info.sql)
     features_tdf = DataFrame.from_query(context.dataset_info.sql)
-    features_pdf = features_tdf.to_pandas(all_rows=True)
+    # features_pdf = features_tdf.to_pandas(all_rows=True)
 
     # Scaling the test set
-    print(f"Loading scaler from table scaler_{context.model_version}")
-    scaler = DataFrame(f"scaler_{context.model_version}")
+    print ("Loading scaler...")
+    scaler = DataFrame(f"scaler_${context.model_version}")
 
+    # Scale the test dataset using the trained scaler
     scaled_test = ScaleTransform(
         data=test_df,
         object=scaler,
-        accumulate=entity_key
+        accumulate = entity_key
     )
 
     print("Scoring...")
+    # Make predictions using the XGBoostPredict function
     predictions = XGBoostPredict(
         object=model,
         newdata=scaled_test.result,
-        model_type='Classification',
+        model_type = 'Classification',
         id_column=entity_key,
         output_prob=True,
-        output_responses=['0', '1'],
-        object_order_column=['task_index', 'tree_num',
-                             'iter', 'class_num', 'tree_order']
+        output_responses=['0','1'],
+        object_order_column=['task_index', 'tree_num', 'iter', 'class_num', 'tree_order']
     )
 
-    predictions_pdf = predictions.result.to_pandas(all_rows=True).rename(
-        columns={"Prediction": target_name}).astype(int)
+
+    # store the predictions
+    predictions_df = predictions.result
+    predictions_pdf = predictions_df.assign(drop_columns=True,
+                                             job_id=translate(context.job_id),
+                                             PatientId=predictions_df.PatientId,
+                                             HasDiabetes=predictions_df.Prediction.cast(type_=INTEGER),
+                                             json_report=translate("  "))
+
+
 
     print("Finished Scoring")
 
-    # store the predictions
-    predictions_pdf = pd.DataFrame(predictions_pdf, columns=[target_name])
-    predictions_pdf[entity_key] = features_pdf.index.values
-    # add job_id column so we know which execution this is from if appended to predictions table
-    predictions_pdf["job_id"] = context.job_id
 
-    # teradataml doesn't match column names on append.. and so to match / use same table schema as for byom predict
-    # example (see README.md), we must add empty json_report column and change column order manually (v17.0.0.4)
-    # CREATE MULTISET TABLE pima_patient_predictions
-    # (
-    #     job_id VARCHAR(255), -- comes from airflow on job execution
-    #     PatientId BIGINT,    -- entity key as it is in the source data
-    #     HasDiabetes BIGINT,   -- if model automatically extracts target
-    #     json_report CLOB(1048544000) CHARACTER SET UNICODE  -- output of
-    # )
-    # PRIMARY INDEX ( job_id );
-    predictions_pdf["json_report"] = ""
-    predictions_pdf = predictions_pdf[[
-        "job_id", entity_key, target_name, "json_report"]]
+#     # teradataml doesn't match column names on append.. and so to match / use same table schema as for byom predict
+#     # example (see README.md), we must add empty json_report column and change column order manually (v17.0.0.4)
+#     # CREATE MULTISET TABLE pima_patient_predictions
+#     # (
+#     #     job_id VARCHAR(255), -- comes from airflow on job execution
+#     #     PatientId BIGINT,    -- entity key as it is in the source data
+#     #     HasDiabetes BIGINT,   -- if model automatically extracts target
+#     #     json_report CLOB(1048544000) CHARACTER SET UNICODE  -- output of
+#     # )
+#     # PRIMARY INDEX ( job_id );
+#     predictions_pdf["json_report"] = ""
+#     predictions_pdf = predictions_pdf[["job_id", entity_key, target_name, "json_report"]]
 
     copy_to_sql(
         df=predictions_pdf,
@@ -92,7 +100,6 @@ def score(context: ModelContext, **kwargs):
             WHERE job_id = '{context.job_id}'
     """)
 
-    record_scoring_stats(features_df=features_tdf,
-                         predicted_df=predictions_df, context=context)
+    record_scoring_stats(features_df=features_tdf, predicted_df=predictions_df, context=context)
 
     print("All done!")
